19d37ee63612da0bc21a3fba60910a108f14c4e4
phoby20 <phoby20@hotmail.com>
2022-07-16 20:28:57 +0900

    商品リストの商品moleculeコンポーネントの作成

--- frontend/mofu_com/src/components/user/organisms/ProductListContents/index.vue
+++ frontend/mofu_com/src/components/user/organisms/ProductListContents/index.vue
@@ -1,9 +1,31 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ProductItem from "../../molecules/ProductItem/index.vue";
+
+type ProductItemProps = {
+  title: string;
+  description: string;
+  url: string;
+  lastUpdate: number;
+}[];
+
+const props = defineProps({
+  data: Array,
+});
+</script>
 
 <template>
-  <div class="container">
-    <h3>商品リストページのorganism component</h3>
+  <div class="row row-cols-1 row-cols-md-3 g-4">
+    <div v-for="item in props.data" :key="item">
+      <div class="product-item">
+        <ProductItem :data="item" />
+      </div>
+    </div>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.product-item:hover {
+  border: 1px solid red;
+  cursor: pointer;
+}
+</style>
