61f934a6b2e19898a221b4e1a9f0dfde76b01929
phoby20 <phoby20@hotmail.com>
2022-07-28 20:43:36 +0900

    mongoDB仮接続及びdocker-composeバージョンアップ

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,52 @@
 const express = require("express");
 const app = express();
 
-const PORT = 8080;
-const HOST = "0.0.0.0";
+// 以下mongoDB接続
+const MongoClient = require("mongodb").MongoClient;
+const uri =
+  "mongodb+srv://admin:romantic17@cluster0.8hbkz.mongodb.net/?retryWrites=true&w=majority";
 
-app.get("/", (req, res) => {
-  res.send("Hello world!");
+const sampleData = {
+  title: "花誕生123",
+  description:
+    "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat dolore nihil minus, officia distinctio doloremque delectus quam mollitia. A, debitis.",
+  url: "../../../../../public/sample_image/sample7.jpeg",
+  price: 70000,
+  lastUpdate: 5,
+};
+
+let db;
+MongoClient.connect(uri, function (dbConnectErr, client) {
+  if (dbConnectErr) return console.log(dbConnectErr);
+
+  db = client.db("mofu_com");
+
+  // 디비에 데이터 저장
+  // db.collection("products").insertOne(
+  //   sampleData,
+  //   function (insertErr, insertRes) {
+  //     if (insertRes) {
+  //       console.log("저장완료");
+  //     } else {
+  //       console.log("저장불가");
+  //     }
+  //   }
+  // );
+
+  const PORT = 8080;
+  const HOST = "0.0.0.0";
+
+  app.get("/api", (req, res) => {
+    db.collection("products")
+      .find()
+      .toArray(function (findErr, findRes) {
+        res.header("Content-Type", "application/json; charset=utf-8");
+        res.send(findRes);
+      });
+  });
+
+  app.listen(PORT, HOST);
+  // client.close();
 });
-
-app.listen(PORT, HOST);
 
 console.log("server is running!");
