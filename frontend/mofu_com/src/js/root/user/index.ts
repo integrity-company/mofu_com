dd682d666c4c7bc62ef8caac6beeab3fdb8dd38c
phoby20 <phoby20@hotmail.com>
2022-07-30 16:30:44 +0900

    rootの基本設定のファイルのpath変更

--- frontend/mofu_com/src/js/root/user/index.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import UserApp from "../../../entry/user/UserApp.vue";
-import { router } from "../../router/user/router";
-
-const app = createApp(UserApp);
-app.use(router);
-
-app.mount("#app");
--- frontend/mofu_com/src/main.ts
+++
@@ -1,5 +0,0 @@
-import { createApp } from "vue";
-import UserApp from "./entry/user/UserApp.vue";
-import { router } from "./js/router/user/router";
-
-createApp(UserApp).use(router).mount("#app");
