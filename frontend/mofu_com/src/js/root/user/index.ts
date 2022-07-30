a21a80d92fc984403d253ed7ec11e2925178a1c6
phoby20 <phoby20@hotmail.com>
2022-07-30 09:31:31 +0900

    rootファイルの追加漏れ対応

---
+++ frontend/mofu_com/src/js/root/user/index.ts
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+import UserApp from "../../../entry/user/UserApp.vue";
+import { router } from "../../router/user/router";
+
+const app = createApp(UserApp);
+app.use(router);
+
+app.mount("#app");
