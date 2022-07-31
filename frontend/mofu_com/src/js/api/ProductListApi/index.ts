0cba7268fa7e3bd607e2ca3dbd9041488eb2ced3
phoby20 <phoby20@hotmail.com>
2022-07-31 21:37:24 +0900

    envの設定及びpackage.jsonのバージョンアップとバージョン固定

--- frontend/mofu_com/src/js/api/ProductListApi/index.ts
+++ frontend/mofu_com/src/js/api/ProductListApi/index.ts
@@ -2,14 +2,14 @@ import axios from 'axios'
 import { ProductList } from '../../type/user/general/products'
 
 const PATH = {
-  SAMPLE_DATA: '/api',
+  PRODUCTS: '/api/products',
 }
 
 type ProductListResponse = ProductList[]
 
 export class ProductListApi {
   public async fetchProductList(): Promise<ProductListResponse> {
-    const { data } = await axios.get(PATH.SAMPLE_DATA)
+    const { data } = await axios.get(PATH.PRODUCTS)
     return data
   }
 }
