ed3beb23a5a7870eeed1b563c8199a4fed2c695a
phoby20 <phoby20@hotmail.com>
2022-08-01 22:17:35 +0900

    modal moleculeの作成

---
+++ frontend/mofu_com/src/components/user/molecules/Modal/index.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="modal" tabindex="-1">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">
+            <slot name="title" />
+          </h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          <p>
+            <slot name="body" />
+          </p>
+        </div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-secondary"
+            data-bs-dismiss="modal"
+          >
+            <slot name="secondary-btn" />
+          </button>
+          <button type="button" class="btn btn-primary">
+            <slot name="primary-btn" />
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
